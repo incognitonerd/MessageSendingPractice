174dcc59f93b53d9ac3b77203aaa842b839dfb4d
incognitonerd <Argos@4k-tv.attlocal.net>
2019-08-11 22:15:15 -0400

    First Commit

---
+++ src/test/java/com/sandbox/runapp/RunApp.java
@@ -0,0 +1,16 @@
+package com.sandbox.runapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunApp {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
