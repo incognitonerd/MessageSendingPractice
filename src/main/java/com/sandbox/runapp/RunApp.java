06ee6e5ddfca01dd576af8ae6bbe15e5dcd6ff9d
incognitonerd <Argos@4k-tv.attlocal.net>
2019-08-11 22:45:53 -0400

    Added logic to send message to queue

--- src/main/java/com/sandbox/runapp/RunApp.java
+++ src/main/java/com/sandbox/runapp/RunApp.java
@@ -2,12 +2,19 @@ package com.sandbox.runapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 public class RunApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunApp.class, args);
+		ApplicationContext ctx = SpringApplication.run(RunApp.class, args);
+		
+		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
+		
+		jms.convertAndSend("practiceQueue", "Lassie");
+		
+		SpringApplication.exit(ctx);
 	}
-
 }
